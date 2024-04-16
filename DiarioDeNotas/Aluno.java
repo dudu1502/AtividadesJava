a085c900c941f39d432d6b5f1bdd3b0e3e2b1650
dudu1502 <eduardo.tavares552@gmail.com>
2024-04-16 20:46:43 -0300

    part 1 atualizada

--- DiarioDeNotas/Aluno.java
+++ DiarioDeNotas/Aluno.java
@@ -5,48 +5,34 @@ public class Aluno {
     int idade;
     int ra;
     String nome;
-    String mail;
-    int telefone;
+    String email;
+    String telefone;
 
-    public void identificarAluno(){
+    public void identificarAluno() {
 
         Scanner sc = new Scanner(System.in);
-            System.out.println("Digite seu nome: ");
-            nome = sc.nextLine();
-            System.out.println("Digite seu RA: ");
-            ra = sc.nextInt();
-            System.out.println("Digite sua idade: ");
-            idade = sc.nextInt();
-            System.out.println("Digite seu email: ");
-            mail = sc.next();
-            System.out.println("Digite seu telefone: ");
-            telefone = sc.nextInt();
-            System.out.println("Seja bem vindo " + nome);
 
-            sc.close();
-        
+        System.out.println("Digite seu nome: ");
+        nome = sc.nextLine();
+
+        System.out.println("Digite seu RA: ");
+        ra = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Digite sua idade: ");
+        idade = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Digite seu email: ");
+        email = sc.nextLine();
+
+        System.out.println("Digite seu telefone: ");
+        telefone = sc.nextLine();
+
+        System.out.println("Seja bem vindo aluno " + nome);
+
+        sc.close();
 
     }
 
-    public int Ra() {
-        return ra;
-    }
-
-    public int Idade() {
-        return idade;
-    }
-
-    public String Nome() {
-        return nome;
-    }
-
-    public String Email() {
-        return email;
-    }
-
-    public int Telefone() {
-        return telefone;
-    }
-
-    
 }
--- DiarioDeNotas/Professor.java
+++ DiarioDeNotas/Professor.java
@@ -3,20 +3,25 @@ import java.util.Scanner;
 public class Professor {
 
     String nome;
-    String mail;
+    String email;
     int idade;
     
 
     public void identificarProfessor() {
 
             Scanner sc = new Scanner(System.in);
+
             System.out.println("Digite seu nome: ");
             nome = sc.nextLine();
+
             System.out.println("Digite sua idade: ");
             idade = sc.nextInt();
+            sc.nextLine();
+
             System.out.println("Digite seu email: ");
-            mail = sc.next();
-            System.out.println("Seja bem vindo " + nome);
+            email = sc.nextLine();
+
+            System.out.println("Seja bem vindo(a) professor " + nome);
 
             sc.close();
 
